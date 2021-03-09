690174625205f1d6cdd46b2b4a9793694e745ab7
Mario Kukutsov <kukucov@gmail.com>
2021-03-09 14:51:53 +0200

    Pre epic fail commit

--- bosh/create-env.sh
+++ bosh/create-env.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 
 STEP() { echo ; echo ; echo "==\\" ; echo "===>" "$@" ; echo "==/" ; echo ; }
 
-bosh_deployment="$(cd "$(dirname "${BASH_SOURCE[0]}")"; cd ..; pwd)"
+bosh_deployment="$(cd "$(dirname "${BASH_SOURCE[0]}")"; cd ..; cd bosh-deployment; pwd)"
 bosh_deployment_sha="$(cd "${bosh_deployment}"; git rev-parse --short HEAD)"
 
 if [ "${PWD##${bosh_deployment}}" != "${PWD}" ] || [ -e virtualbox/create-env.sh ] || [ -e ../virtualbox/create-env.sh ]; then
@@ -21,18 +21,17 @@ STEP "Creating BOSH Director"
 
 bosh create-env "${bosh_deployment}/bosh.yml" \
   --state "${PWD}/state.json" \
+  --var-file "${PWD}/landscape-local-vars.yml"
   --ops-file "${bosh_deployment}/virtualbox/cpi.yml" \
   --ops-file "${bosh_deployment}/virtualbox/outbound-network.yml" \
   --ops-file "${bosh_deployment}/bosh-lite.yml" \
   --ops-file "${bosh_deployment}/uaa.yml" \
   --ops-file "${bosh_deployment}/credhub.yml" \
   --ops-file "${bosh_deployment}/jumpbox-user.yml" \
+  --ops-file "${bosh_deployment}/use-bionic.yml" \
+  --ops-file "${PWD}/landscape-local.yml" \
   --vars-store "${PWD}/creds.yml" \
-  --var director_name=bosh-lite \
-  --var internal_ip=192.168.50.6 \
-  --var internal_gw=192.168.50.1 \
-  --var internal_cidr=192.168.50.0/24 \
-  --var outbound_network_name=NatNetwork "$@"
+
 
 
 ####
